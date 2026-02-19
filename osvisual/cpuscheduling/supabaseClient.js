4e0132c8af04b3559eb4cfc3e588e962ff787ec9
SaraPonkshe2104 <sara.a.ponkshe@gmail.com>
2026-02-20 05:14:06 +0530

    os

--- osvisual/cpuscheduling/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-export const supabase = createClient(
-  "https://petdylyqzjpeczvutpys.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBldGR5bHlxempwZWN6dnV0cHlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzMzY4MjcsImV4cCI6MjA4NjkxMjgyN30.Ftk9_TpBGjEUG_TGDIHVGKvVCkWim5d-ztBkPN63WPA"
-);
--- osvisual/partials/_dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-//SCRIPT TO CONNECT TO THE DATABASE
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "forum";
-$conn = mysqli_connect($servername, $username, $password, $database);
-?>
